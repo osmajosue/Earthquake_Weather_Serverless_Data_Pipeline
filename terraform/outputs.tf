23c77151bbf5a5b30b6a06287c87d47c7db490b5
Osmani Diaz <osmadc005@gmail.com>
2025-04-11 17:51:56 -0400

    Update terraform files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,29 @@
-output "raw_bucket_name" {
-  description = "S3 bucket for raw data"
-  value       = aws_s3_bucket.nba_raw.bucket
+output "raw_data_bucket" {
+  description = "S3 bucket for raw NBA data"
+  value       = aws_s3_bucket.raw_data.bucket
 }
 
-output "processed_bucket_name" {
-  description = "S3 bucket for processed data"
-  value       = aws_s3_bucket.nba_processed.bucket
+output "processed_data_bucket" {
+  description = "S3 bucket for processed NBA data"
+  value       = aws_s3_bucket.processed_data.bucket
+}
+
+output "scripts_bucket" {
+  description = "S3 bucket for Glue PySpark scripts"
+  value       = aws_s3_bucket.script_bucket.bucket
+}
+
+output "firehose_stream_name" {
+  description = "Name of the Kinesis Firehose delivery stream"
+  value       = aws_kinesis_firehose_delivery_stream.nba_stream.name
 }
 
 output "lambda_function_name" {
-  description = "Name of the NBA ingestion Lambda function"
-  value       = aws_lambda_function.nba_ingestor.function_name
+  description = "Lambda function that fetches NBA data"
+  value       = aws_lambda_function.nba_lambda.function_name
+}
+
+output "glue_job_name" {
+  description = "AWS Glue job for transforming NBA data"
+  value       = aws_glue_job.nba_transform.name
 }
